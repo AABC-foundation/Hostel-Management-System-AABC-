1dece7d18066bf0aa21c5d4c9d44867b48808249
AABC-foundation <77949766+AABC-foundation@users.noreply.github.com>
2021-01-25 13:21:51 +0900

    Update aabc.sol

--- aabc.sol
+++ aabc.sol
@@ -10,7 +10,7 @@ import "./ERC20Detailed.sol";
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `ERC20` functions.
  */
-contract BalloonCoinX is Context, ERC20, ERC20Detailed {
+contract Asset and Arbitrage Coin is Context, ERC20, ERC20Detailed {
     uint8 public constant DECIMALS = 6;
     uint256 public constant INITIAL_SUPPLY = 30000000000 * (10 ** uint256(DECIMALS));
     /**
